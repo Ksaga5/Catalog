fe498bda8115708981f72142edd9cd4dfb66805f
Ksaga <52404149+Ksaga5@users.noreply.github.com>
2021-12-20 14:24:33 +0300

    Project 1

---
+++ constants/catalog.js
@@ -0,0 +1,65 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'Nike Air Max 90 FlyEase',
+        img: 'img/catalog/Nike Air Max 90 FlyEase.jpg',
+        price: 11500,
+
+    },
+    {
+        id: 'el2',
+        name: 'Nike React Infinity Run Flyknit',
+        img: 'img/catalog/Nike React Infinity Run Flyknit.jpg',
+        price: 12790,
+
+    },
+    {
+        id: 'el3',
+        name: 'Nike Zoom Fly 3 Premium',
+        img: 'img/catalog/Nike Zoom Fly 3 Premium.jpg',
+        price: 12490,
+
+    },
+    {
+        id: 'el4',
+        name: 'Jordan Air Max 200 Paris Saint-Germain',
+        img: 'img/catalog/Jordan Air Max 200 Paris Saint-Germain.jpg',
+        price: 11790,
+
+    },
+    {
+        id: 'el5',
+        name: 'Nike Mercurial Vapor 13 Elite FG',
+        img: 'img/catalog/Nike Mercurial Vapor 13 Elite FG.jpg',
+        price: 19490,
+
+    },
+    {
+        id: 'el6',
+        name: 'Nike Mercurial Superfly 7 Elite FG',
+        img: 'img/catalog/Nike Mercurial Superfly 7 Elite FG.jpg',
+        price: 19990,
+
+    },
+    {
+        id: 'el7',
+        name: 'Nike Air Zoom SuperRep',
+        img: 'img/catalog/Nike Air Zoom SuperRep.jpg',
+        price: 9490,
+
+    },
+    {
+        id: 'el8',
+        name: 'Nike Air VaporMax 360',
+        img: 'img/catalog/Nike Air VaporMax 360.jpg',
+        price: 17490,
+
+    },
+    {
+        id: 'el9',
+        name: 'Nike Air Max Plus III',
+        img: 'img/catalog/Nike Air Max Plus III.jpg',
+        price: 14990,
+
+    }
+];
